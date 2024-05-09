0eba51e3a3db3521f5dfe5347f990a97d88d9bd8
Arnav K <arnavkaushal09@gmail.com>
2024-05-09 17:49:58 +0000

    add readme

--- npm/index.js
+++ npm/index.js
@@ -2,19 +2,20 @@
 
 const { spawnSync } = require("child_process");
 
-
 function getExecutablePath() {
   const arch = process.arch;
   let os = process.platform;
   let extension = "";
-  
+
   if (["win32", "cygwin"].includes(process.platform)) {
     os = "windows";
     extension = ".exe";
   }
 
   try {
-    return require.resolve(`rust_cli_for_npx-${os}-${arch}/bin/rust_cli_for_npx${extension}`);
+    return require.resolve(
+      `rust_cli_for_npx-${os}-${arch}/bin/rust_cli_for_npx${extension}`,
+    );
   } catch {
     throw new Error(
       `Couldn't find application binary inside node_modules for ${os}-${arch}`,
@@ -24,7 +25,9 @@ function getExecutablePath() {
 
 function main() {
   const args = process.argv.slice(2);
-  const processResult = spawnSync(getExecutablePath(), args, { stdio: "inherit" });
+  const processResult = spawnSync(getExecutablePath(), args, {
+    stdio: "inherit",
+  });
   process.exit(processResult.status ?? 0);
 }
 
